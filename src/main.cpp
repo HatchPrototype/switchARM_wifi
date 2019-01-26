1ad04fceff7e64a58de3c49e946ef8b5a42c5421
HatchPrototype <support@hatchprototype.comm>
2019-01-26 22:39:23 +0530

    added rensponse for getting switch state and current RGB

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@
 WiFiClient client;
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
 Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "switches/sw1");
+Adafruit_MQTT_Publish photocell = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "switches/sw1_out");
 String vals[10];
 bool dataValid = false;
 byte outBuff[] = {0xCC,0xAA,0xCC,0xAA,0x00,0x00,0x00,0x00,0x00,0x00};
@@ -53,6 +54,17 @@ byte calcCRC()
 
   return cr;
 }
+
+
+void sendStuff(String gt)
+{
+  if (! photocell.publish(gt.c_str())) {
+  } else {
+
+  }
+}
+
+
 
 
 int splitString(String inpu, char delim)
@@ -224,9 +236,115 @@ void loop() {
               }
 
             }
+
+            else if(vals[1] == "GETST")
+            {
+              outBuff[4] = 0x04;
+              outBuff[5] = 0x00;
+              outBuff[6] = 0x00;
+              outBuff[7] = 0x00;
+              outBuff[8] = 0x00;
+              outBuff[9] = calcCRC();
+              Serial.write(outBuff,10);
+            }
+            else if(vals[1] == "GETRGB")
+            {
+              outBuff[4] = 0x05;
+              outBuff[5] = 0x00;
+              outBuff[6] = 0x00;
+              outBuff[7] = 0x00;
+              outBuff[8] = 0x00;
+              outBuff[9] = calcCRC();
+              Serial.write(outBuff,10);
+            }
           }
       }
   }
 }
-  delay(5);
+byte df[12];
+Serial.setTimeout(5);
+if(Serial.available())
+{
+  Serial.readBytes(df,11);
+  byte crc = 0x00;
+  for(int x=0;x<10;x++)
+  {
+    crc ^= df[x];
+  }
+  if(df[11] == crc)
+  {
+    if((df[0]==0xCC)&&(df[1]==0xAA)&&(df[2]==0xCC)&&(df[3]==0xAA))
+    {
+      if(df[4] == 0xF0)
+      {
+        String outst = "SWITCH,ST,";
+        if(df[5]==0x00)
+        {
+          outst = outst + "0,";
+        }
+        else if(df[5]==0x01)
+        {
+          outst = outst + "1,";
+        }
+        else
+        {
+          outst = outst + "2,";
+        }
+
+        if(df[6]==0x00)
+        {
+          outst = outst + "0,";
+        }
+        else if(df[6]==0x01)
+        {
+          outst = outst + "1,";
+        }
+        else
+        {
+          outst = outst + "2,";
+        }
+
+        if(df[7]==0x00)
+        {
+          outst = outst + "0,";
+        }
+        else if(df[7]==0x01)
+        {
+          outst = outst + "1,";
+        }
+        else
+        {
+          outst = outst + "2,";
+        }
+
+        if(df[8]==0x00)
+        {
+          outst = outst + "0";
+        }
+        else if(df[8]==0x01)
+        {
+          outst = outst + "1";
+        }
+        else
+        {
+          outst = outst + "2";
+        }
+        sendStuff(outst);
+
+      }
+      else if(df[4] == 0xF2)
+      {
+        String outst = "SWITCH,RGB,";
+        outst = outst + String((int) df[5]) + ",";
+        outst = outst + String((int) df[6]) + ",";
+        outst = outst + String((int) df[7]) + ",";
+        outst = outst + String((int) df[8]) + ",";
+        outst = outst + String((int) df[9]) + ",";
+        outst = outst + String((int) df[10]);
+        sendStuff(outst);
+      }
+    }
+  }
+
+}
 }
